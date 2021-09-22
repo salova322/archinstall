e3a7e2112cad71668a5de836fd508dc657faea62
salova322 <33322582+salova322@users.noreply.github.com>
2021-09-22 15:16:29 +0000

    Update archinstall.sh

--- archinstall.sh
+++ archinstall.sh
@@ -1,3 +1,6 @@
+
+cfdisk
+
 timedatectl set-ntp true
 
 mkfs.fat -F32 /dev/sda1
@@ -13,43 +16,3 @@ genfstab -U /mnt >> /mnt/etc/fstab
 
 arch-chroot /mnt
 
-fallocate -l 2GB /swapfile
-chmod 600 /swapfile
-mkswap /swapfile
-swapon /swapfile
-#nano /etc/fstab
-echo '/swapfile none swap 0 0' >> /etc/fstab
-
-ln -sf /usr/share/zoneinfo/Asia/Yekaterinburg /etc/localtime
-hwclock --sysohc
-
-nano /etc/locale.gen
-locale-gen
-#nano /etc/locale.conf
-echo 'LANG=en_US.UTF-8' >> /etc/locale.conf
-
-#nano /etc/hostname
-echo 'thinkpad' >> /etc/hostname
-
-#nano /etc/hosts
-echo '127.0.0.1   localhost \n::1         localhost \n127.0.1.1   thinkpad.localdomain    thinkpad' >> /etc/hosts
-
-passwd
-
-pacman -S grub networkmanager nano file-roller zip unzip network-manager-applet wireless_tools wpa_supplicant dialog mtools dosfstools base-devel linux-headers pulseaudio pavucontrol alsa-utils
-
-grub-install /dev/sda
-grub-mkconfig -o /boot/grub/grub.cfg 
-
-pacman -S xf86-video-intel xorg xfce4 xfce4-goodies lightdm  lightdm-gtk-greeter lightdm-gtk-greeter-settings xdg-user-dirs
-
-systemctl enable lightdm
-systemctl enable NetworkManager
-
-useradd -m -G wheel salova
-passwd salova 
-EDITOR=nano visudo
-
-exit 
-umount -a
-reboot
