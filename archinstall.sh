3ad18e24607c13270bdf257cac354696318f7bfc
salova322 <33322582+salova322@users.noreply.github.com>
2021-09-22 15:24:22 +0000

    Update archinstall.sh

--- archinstall.sh
+++ archinstall.sh
@@ -11,9 +11,11 @@ mount /dev/sda2 /mnt
 mkdir -p /mnt/boot
 mount /dev/sda1 /mnt/boot
 
-pacstrap /mnt base linux linux-firmware  
+pacstrap /mnt base linux linux-firmware git nano 
 
 genfstab -U /mnt >> /mnt/etc/fstab
+
+cp -r archinstall/chroot.sh /mnt 
 
 arch-chroot /mnt
 
